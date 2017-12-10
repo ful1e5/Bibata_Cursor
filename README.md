ce3eb72b09e9eccc8420e9374cd251638b45e672
KaizIqbal <KaizIqbal@users.noreply.github.com>
2017-12-10 09:37:18 +0530

    Flavours Added

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # Bibata_Cursor
-###### Material Based Cursor 
+###### Material Based Cursor
 ![alt text](https://github.com/KaizIqbal/Bibata_Cursor/blob/master/image/wall.png)
+# Flavours
+Now Bibata Cursor Is Available In Two Theme Light & Dark.
+
+## Bibata Oil
+###### Dark Theme(Default)
+ ![alt text](https://github.com/KaizIqbal/Bibata_Cursor/blob/master/image/Bibata_Oil_all.png)
+ ## Bibata Ice
+ ###### Light Theme
+  ![alt text](https://github.com/KaizIqbal/Bibata_Cursor/blob/master/image/Bibata_ice_all.png)
 # Installation
 The Installation Is Easy,You Just Follow This Command
 
       $ git clone https://github.com/KaizIqbal/Bibata_Cursor.git
       $ cd Bibata_Cursor/
-      $ sudo chmod +x Bibata_installer.sh
-      $ sudo ./Bibata_installer.sh 
-If You get any error Like missing cursor,So You must Have To rebuild The cursor By Following Command 
+      $ sudo chmod +x ./Installer_Bibata.sh
+      $ sudo ./Installer_Bibata.sh  
+If You get any error Like missing cursor,So You must Have To rebuild The cursor By Following Command
 
-      $ sudo chmod +x $ ./build.sh 
-      $ ./build.sh 
+      $ sudo chmod +x $ ./build.sh
+      $ ./build.sh
 
 ![alt text](https://github.com/KaizIqbal/Bibata_Cursor/blob/master/image/all.png)
 # Build
@@ -19,13 +28,8 @@ If You get any error Like missing cursor,So You must Have To rebuild The cursor 
 ### Inkscape
 ### xcursorgen
 
-###### How To Build
-In The ['src'](https://github.com/KaizIqbal/Bibata_Cursor/tree/master/src) Directory you see all the Source File.It Also Contain Oe Directory Called ['svgs'](https://github.com/KaizIqbal/Bibata_Cursor/tree/master/src/Bibata/svgs) That Contain All .svg File 
+###### How I Contribute The Bibata Project
+In The ['src'](https://github.com/KaizIqbal/Bibata_Cursor/tree/master/src) Directory you see all the Source File.It Also Contain One Directory Called ['svgs'](https://github.com/KaizIqbal/Bibata_Cursor/tree/master/src/Bibata/svgs) That Contain All .svg File Edit It Using Inkscape or another Tool you Used,And Contribute The Project
 
-
-###### After Buil The Icons Using Following Commands
-
-      $ sudo chmod +x build.sh
-      $ ./build.sh
 
 ENJOY.
--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
-RAWSVGS_Bibata="src/Bibata/svgs"
-INDEX_Bibata="src/Bibata/cursor.theme"
-INDEX1_Bibata="src/Bibata/index.theme"
+
+# Bibata Default
+RAWSVGS_Bibata="src/Bibata_Oil/svgs"
+INDEX_Bibata="src/Bibata_Oil/cursor.theme"
+INDEX1_Bibata="src/Bibata_Oil/index.theme"
+
+# Bibata Light
+
+RAWSVGS_Bibata_Light="src/Bibata_Ice/svgs"
+INDEX_Bibata_Light="src/Bibata_Ice/cursor.theme"
+INDEX1_Bibata_Light="src/Bibata_Ice/index.theme"
+
 ALIASES="src/cursorList"
 
 
@@ -23,13 +32,21 @@ echo -e "Checking Requirements... DONE"
 
 
 echo -ne "Making Folders... $BASENAME\\r"
-DIR2X_Bibata="build/Bibata/x2"
-DIR1X_Bibata="build/Bibata/x1"
+DIR2X_Bibata="build/Bibata_Oil/x2"
+DIR1X_Bibata="build/Bibata_Oil/x1"
+DIR2X_Bibata_Light="build/Bibata_Ice/x2"
+DIR1X_Bibata_Light="build/Bibata_Ice/x1"
+
 OUTPUT_Bibata="$(grep --only-matching --perl-regex "(?<=Name\=).*$" $INDEX_Bibata)"
 OUTPUT_Bibata=${OUTPUT_Bibata// /_}
+OUTPUT_Bibata_Light="$(grep --only-matching --perl-regex "(?<=Name\=).*$" $INDEX_Bibata_Light)"
+OUTPUT_Bibata_Light=${OUTPUT_Bibata_Light// /_}
 mkdir -p "$DIR2X_Bibata"
 mkdir -p "$DIR1X_Bibata"
+mkdir -p "$DIR2X_Bibata_Light"
+mkdir -p "$DIR1X_Bibata_Light"
 mkdir -p "$OUTPUT_Bibata/cursors"
+mkdir -p "$OUTPUT_Bibata_Light/cursors"
 echo 'Making Folders... DONE';
 
 
@@ -40,8 +57,10 @@ for CUR in src/config/*.cursor; do
 
     echo -ne "\033[0KGenerating simple cursor pixmaps... $BASENAME\\r"
 
-    inkscape -w 32  -f $RAWSVGS_Bibata/"$BASENAME".svg -e "$DIR1X_Bibata/$BASENAME.png" > /dev/null
-    inkscape -w 64 -f $RAWSVGS_Bibata/"$BASENAME".svg -e "$DIR2X_Bibata/$BASENAME.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata/"$BASENAME".svg -e "$DIR1X_Bibata/$BASENAME.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata/"$BASENAME".svg -e "$DIR2X_Bibata/$BASENAME.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata_Light/"$BASENAME".svg -e "$DIR1X_Bibata_Light/$BASENAME.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata_Light/"$BASENAME".svg -e "$DIR2X_Bibata_Light/$BASENAME.png" > /dev/null
 done
 echo -e "\033[0KGenerating simple cursor pixmaps... DONE"
 
@@ -51,11 +70,15 @@ for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
     echo -ne "\033[0KGenerating animated cursor pixmaps... $i / 45 \\r"
 
-    inkscape -w 32  -f $RAWSVGS_Bibata/progress-$i.svg -e "$DIR1X_Bibata/progress-$i.png" > /dev/null
-    inkscape -w 64 -f $RAWSVGS_Bibata/progress-$i.svg -e "$DIR2X_Bibata/progress-$i.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata/progress-$i.svg -e "$DIR1X_Bibata/progress-$i.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata/progress-$i.svg -e "$DIR2X_Bibata/progress-$i.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata_Light/progress-$i.svg -e "$DIR1X_Bibata_Light/progress-$i.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata_Light/progress-$i.svg -e "$DIR2X_Bibata_Light/progress-$i.png" > /dev/null
 
-    inkscape -w 32  -f $RAWSVGS_Bibata/wait-$i.svg -e "$DIR1X_Bibata/wait-$i.png" > /dev/null
-    inkscape -w 64 -f $RAWSVGS_Bibata/wait-$i.svg -e "$DIR2X_Bibata/wait-$i.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata/wait-$i.svg -e "$DIR1X_Bibata/wait-$i.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata/wait-$i.svg -e "$DIR2X_Bibata/wait-$i.png" > /dev/null
+    inkscape -w 33  -f $RAWSVGS_Bibata_Light/wait-$i.svg -e "$DIR1X_Bibata_Light/wait-$i.png" > /dev/null
+    inkscape -w 66 -f $RAWSVGS_Bibata_Light/wait-$i.svg -e "$DIR2X_Bibata_Light/wait-$i.png" > /dev/null
 done
 echo -e "\033[0KGenerating animated cursor pixmaps... DONE"
 
@@ -67,11 +90,18 @@ for CUR in src/config/*.cursor; do
     BASENAME=${BASENAME##*/}
     BASENAME=${BASENAME%.*}
 
-    ERR="$( xcursorgen -p build/Bibata "$CUR" "$OUTPUT_Bibata/cursors/$BASENAME" 2>&1 )"
+    ERR="$( xcursorgen -p build/Bibata_Oil "$CUR" "$OUTPUT_Bibata/cursors/$BASENAME" 2>&1 )"
     
     if [[ "$?" -ne "0" ]]; then
         echo "FAIL: $CUR $ERR"
     fi
+
+    ERR="$( xcursorgen -p build/Bibata_Ice "$CUR" "$OUTPUT_Bibata_Light/cursors/$BASENAME" 2>&1 )"
+    
+    if [[ "$?" -ne "0" ]]; then
+        echo "FAIL: $CUR $ERR"
+    fi
+
 done
 echo -e "Generating cursor theme... DONE"
 
@@ -84,9 +114,16 @@ while read -r ALIAS ; do
     if [ -e "$OUTPUT_Bibata/cursors/$FROM" ] ; then
         continue
     fi
-    ln -sf "$TO" "$OUTPUT_Bibata/cursors/$FROM"
+    ln -s "$TO" "$OUTPUT_Bibata/cursors/$FROM"
+
+    if [ -e "$OUTPUT_Bibata_Light/cursors/$FROM" ] ; then
+        continue
+    fi
+    ln -s "$TO" "$OUTPUT_Bibata_Light/cursors/$FROM"
+
 done < $ALIASES
 echo -e "\033[0KGenerating shortcuts... DONE"
+
 
 echo -ne "Copying Theme Index...\\r"
 
@@ -94,33 +131,38 @@ echo -ne "Copying Theme Index...\\r"
         cp $INDEX_Bibata "$OUTPUT_Bibata/cursor.theme"
         cp $INDEX1_Bibata "$OUTPUT_Bibata/index.theme"
     fi
+
+    if ! [ -e "$OUTPUT_Bibata_Light/$INDEX_Bibata_Light" ] ; then
+        cp $INDEX_Bibata_Light "$OUTPUT_Bibata_Light/cursor.theme"
+        cp $INDEX1_Bibata_Light "$OUTPUT_Bibata_Light/index.theme"
+    fi
 echo -e "\033[0KCopying Theme Index... DONE"
 
 
 echo -ne "Generating Installer...\\r"
     if ! [ -e "$OUTPUT_Bibata/$INDEX_Bibata" ] ; then
         cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
-        cp src/in.inst Bibata_installer.sh
+        cp src/in.inst Installer_Bibata.sh
     fi
 echo -e "\033[0KGenerating Installer... DONE"
 
 echo -ne "Making Installer Executable...\\r"
     if ! [ -e "$OUTPUT_Bibata/$INDEX_Bibata" ] ; then
         cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
-        gksu chmod +x Bibata_installer.sh
+        gksu chmod +x Installer_Bibata.sh
     fi
 
 show_Msg() {
   echo -e "\033[1;34m$@\033[0m"
 }
 show_command() {
-  echo -e "\033[1;37m$@\033[0m"
+  echo -e "\033[1;93m$@\033[0m"
 }
 
 echo -e "\033[0KMaking Installer Executable... DONE"
 
 show_Msg "For Installation Use Following Command:\n\n"
-show_command "\t sudo ./Bibata_installer.sh\n"
+show_command "\t sudo ./Installer_Bibata.sh\n"
 exit
 
 echo "COMPLETE!"
