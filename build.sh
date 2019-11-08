ec3a18e3eb58c882aed8082b231adc098990adca
KAiZ <24286590+KaizIqbal@users.noreply.github.com>
2019-11-08 10:42:23 +0530

    shell updated

--- build.sh
+++ build.sh
@@ -1,20 +1,124 @@
 #!/bin/bash
+#
+#Bibata Build
+#
+#Released under the GNU General Public License, version 3.
+#Author : KAiZ
+#
 
+echo -e "\n"
+echo -e " ██████╗ ██╗██████╗  █████╗ ████████╗ █████╗   "
+echo -e " ██╔══██╗██║██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗ "
+echo -e " ██████╔╝██║██████╔╝███████║   ██║   ███████║  "
+echo -e " ██╔══██╗██║██╔══██╗██╔══██║   ██║   ██╔══██║  "
+echo -e " ██████╔╝██║██████╔╝██║  ██║   ██║   ██║  ██║  "
+echo -e " ╚═════╝ ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝  "
+echo -e "\n"
+
+#functions
 #Color print function 
 show_Msg() {
-    echo -e "\033[1;34m$@\033[0m"
+    echo -e "\033[1;37m$@\033[0m"
 }
-show_command() {
-    echo -e "\033[1;93m$@\033[0m"
+show_pre(){
+    echo -ne "\033[1;33m$@\033[0m"
 }
 show(){
-    echo -e "\033[0;34m$@\033[0m"
+    echo -e "\033[1;32m$@\033[0m"
+}
+selection(){
+    echo -e "\033[1;36m$@\033[0m"
 }
 error(){
-    echo -e "\033[0;31m$@\033[0m"
+    echo -e "\033[1;31m$@\033[0m"
 }
 
-echo -ne "Checking Requirements...\\r"
+selectWithDefault() {
+
+    local item i=0 numItems=$# 
+
+    # Print numbered menu items, based on the arguments passed.
+    for item; do         # Short for: for item in "$@"; do
+    printf '%s\n' "$((++i))) $item"
+    done >&2 # Print to stderr, as `select` does.
+
+    # Prompt the user for the index of the desired item.
+    while :; do
+    printf %s "${PS3-#? }" >&2 # Print the prompt string to stderr, as `select` does.
+    read -r index
+    # Make sure that the input is either empty or that a valid index was entered.
+    [[ -z $index ]] && break  # empty input
+    (( index >= 1 && index <= numItems )) 2>/dev/null || { echo "Invalid selection. Please try again." >&2; continue; }
+    break
+    done
+
+    # Output the selected item, if any.
+    [[ -n $index ]] && printf %s "${@: index:1}"
+
+}
+
+build(){
+    #get name of theme by argument 
+    cursor=$1
+
+    #building cursor with python script
+    show_pre "\n$cursor : Generating bitmaps...\\r"
+
+    # if cursors source folder & file doesn't exist
+    if [ ! "src/$cursor/source-cursors.svg" ]; 
+    then
+        error "\n$cursor : Source not found"
+        error "\nAborting..."
+        exit 1
+    else
+        #-o for genrating hotspots
+        #-a for genrating config files
+        python render-cursors.py ./src/$cursor/source-cursors.svg -o -a --name $cursor
+        # $? =  is the exit status of the most recently-executed command; by convention, 0 means success and anything else indicates failure. 
+        if [ $? -eq 0 ]
+        then
+            show "$cursor : Generating bitmaps... DONE"
+        else
+            error "$cursor : Generating bitmaps... FAIL"
+            error "\nAborting..."
+            exit 1
+        fi
+
+        show_pre "\n$cursor : Building X11 cursor...\\r"
+        #execute x11-make.sh file with theme_name argument
+        sh x11-make.sh "$cursor"
+        #Copy .index files to out/$cursor
+        cp src/"$cursor"/*.theme "$cursor"/out/X11/"$cursor"
+        if [ $? -eq 0 ]
+        then
+            show "$cursor : Building X11 cursor... DONE"
+            echo "OUT: $PWD/$cursor/out/X11"
+        else
+            error "$cursor : Building X11 cursor... FAIL"
+            error "\nAborting..."
+            exit 1
+        fi
+
+        show_pre "\n$cursor : Building Window cursor...\\r"
+        #execute x11-make.sh file with theme_name argument
+        sh w32-make.sh "$cursor"
+        if [ $? -eq 0 ]
+        then
+            show "$cursor : Building Window cursor... DONE"
+            echo "OUT: $PWD/$cursor/out/win"
+        else
+            error "$cursor : Building Window cursor... FAIL"
+            error "\nAborting..."
+            exit 1
+        fi
+    fi
+    
+}
+
+#main program
+
+#Requirment checking
+show_pre "Checking Requirements...\\r"
 
 if  ! type "inkscape" > /dev/null ; then
     error "\\nFAIL: inkscape must be installed"
@@ -27,43 +131,82 @@ if  ! type "xcursorgen" > /dev/null ; then
 fi
 
 if ! command -v python3 &>/dev/null; then
-    erroe "\\nFAIL: Python 3 must be installed"
+    error "\\nFAIL: python3 must be installed"
+    exit 1
 fi
-echo -e "Checking Requirements... DONE"
+
+if ! command pip &>/dev/null; then
+    error "\\nFAIL: pip must be installed"
+    exit 1
+fi
+show "Checking Requirements... DONE"
+
+#Install pip requirments
+show_pre "Installing PiP Requirements...\\r"
+
+if [ ! "requirements.txt" ]; 
+    then
+        error "\n$cursor : requirements.txt not found"
+        error "\nAborting..."
+        exit 1
+else
+    pip3 install -r requirements.txt --user 
+fi
+
+if [ $? -eq 0 ]
+then
+    show "Installing PiP Requirements... DONE"
+else
+    error "Installing PiP Requirements... FAIL"
+    error "\nAborting..."
+    exit 1
+fi
 
 
+#choice for build cursor
+selection "Cursor to build (Default is 'ALL')?"
+cursors=("Bibata Oil" "Bibata Ice" "Bibata Amber" "test" "ALL"  exit )
+cursor=$(selectWithDefault "${cursors[@]}")
 
+# Process the selected item.
+case $cursor in
+  (''|'ALL') echo "ALL"; ;;
+  ('Bibata Oil') build "$cursor"; ;;
+  ('Bibata Ice') build "$cursor"; ;;
+  ('Bibata Amber') build "$cursor"; ;;
+  ('test') build "$cursor"; ;;
+esac
 
-echo -ne "Generating Installer...\\r"
-    if ! [ -e "$OUTPUT_Bibata/$INDEX_Bibata" ] ; then
-        cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
-        cp src/in.inst Installer_Bibata.sh
-    fi
-echo -e "\033[0KGenerating Installer... DONE"
+show_pre "Generating Installer...\\r"
 
-echo -ne "Making Installer Executable...\\r"
-    if ! [ -e "$OUTPUT_Bibata/$INDEX_Bibata" ] ; then
-        cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
-        gksu chmod +x Installer_Bibata.sh
-    fi
+if [ "src/in.inst" ]; then
+	cp src/in.inst Bibata.sh
+fi
 
-show_Msg() {
-    echo -e "\033[1;34m$@\033[0m"
-}
-show_command() {
-    echo -e "\033[1;93m$@\033[0m"
-}
-show(){
-    echo -e "\033[0;34m$@\033[0m"
-}
-error(){
-    echo -e "\033[0;31m$@\033[0m"
-}
+if [ $? -eq 0 ]
+then
+    show "Generating Installer... DONE"
+else
+    error "Generating Installer... FAIL"
+    error "\nAborting..."
+    exit 1
+fi
 
-echo -e "\033[0KMaking Installer Executable... DONE"
+show_pre "Making Installer Executable...\n"
 
-show_Msg "For Installation Use Following Command:\n\n"
-show_command "\t sudo ./Installer_Bibata.sh\n"
-exit
+if [ "Bibata.sh" ]; then
+	sudo chmod +x Bibata.sh
+fi
 
-echo "COMPLETE!"
+if [ $? -eq 0 ]
+then
+    show "Making Installer Executable... DONE"
+    show_Msg "For Installation Use Following Command:\n"
+    show_Msg "\t sudo ./Bibata.sh\n\t\tOR\n\t./Bibata.sh"
+else
+    error "Making Installer Executable... FAIL"
+    error "\nAborting..."
+    exit 1
+fi
+
+show_Msg "\n🎉 BUILD COMPLETE! 🎉"
--- x11-make.sh
+++ x11-make.sh
@@ -3,77 +3,146 @@
 #get theme name from argument
 THEME=$1
 #current Directory path
-DIR="../"
+DIR="../.."
 
 # enter bitmaps folder
-cd bitmaps
+cd $1/bitmaps
 
 # if cursors folder doesn't exist
-if [ ! -d "$DIR/$THEME/cursors" ]; then
-	mkdir -p $DIR/$THEME/cursors
+if [ ! -d "$DIR/"$THEME"/out/X11/"$THEME"/cursors" ]; then
+	mkdir -p $DIR/"$THEME"/out/X11/"$THEME"/cursors
 fi
 
-xcursorgen all-scroll.in $DIR/$THEME/cursors/all-scroll
-# xcursorgen based_arrow_down.in $DIR/$THEME/cursors/based_arrow_down
-# xcursorgen based_arrow_up.in $DIR/$THEME/cursors/based_arrow_up
-xcursorgen bd_double_arrow.in $DIR/$THEME/cursors/bd_double_arrow
-xcursorgen bottom_left_corner.in $DIR/$THEME/cursors/bottom_left_corner
-xcursorgen bottom_right_corner.in $DIR/$THEME/cursors/bottom_right_corner
-xcursorgen bottom_side.in $DIR/$THEME/cursors/bottom_side
-xcursorgen bottom_tee.in $DIR/$THEME/cursors/bottom_tee
-# xcursorgen center_ptr.in $DIR/$THEME/cursors/center_ptr
-xcursorgen circle.in $DIR/$THEME/cursors/circle
-xcursorgen context-menu.in $DIR/$THEME/cursors/context-menu
-xcursorgen copy.in $DIR/$THEME/cursors/copy
-xcursorgen cross.in $DIR/$THEME/cursors/cross
-xcursorgen crossed_circle.in $DIR/$THEME/cursors/crossed_circle
-xcursorgen crosshair.in $DIR/$THEME/cursors/cell
-# xcursorgen crosshair.in $DIR/$THEME/cursors/crosshair
-xcursorgen dnd-ask.in $DIR/$THEME/cursors/dnd-ask
-xcursorgen dnd-copy.in $DIR/$THEME/cursors/dnd-copy
-xcursorgen dnd-link.in $DIR/$THEME/cursors/dnd-link
-xcursorgen dnd-move.in $DIR/$THEME/cursors/dnd-move
-xcursorgen dnd-no-drop.in $DIR/$THEME/cursors/dnd-no-drop
-xcursorgen dnd-none.in $DIR/$THEME/cursors/dnd-none
-xcursorgen dotbox.in $DIR/$THEME/cursors/dotbox
-xcursorgen fd_double_arrow.in $DIR/$THEME/cursors/fd_double_arrow
-xcursorgen grabbing.in $DIR/$THEME/cursors/grabbing
-xcursorgen hand1.in $DIR/$THEME/cursors/hand1
-xcursorgen hand2.in $DIR/$THEME/cursors/hand2
-xcursorgen left_ptr.in $DIR/$THEME/cursors/left_ptr
-xcursorgen left_ptr_watch.in $DIR/$THEME/cursors/left_ptr_watch
-xcursorgen left_side.in $DIR/$THEME/cursors/left_side
-xcursorgen left_tee.in $DIR/$THEME/cursors/left_tee
-xcursorgen link.in $DIR/$THEME/cursors/link
-xcursorgen ll_angle.in $DIR/$THEME/cursors/ll_angle
-xcursorgen lr_angle.in $DIR/$THEME/cursors/lr_angle
-xcursorgen move.in $DIR/$THEME/cursors/move
-xcursorgen pencil.in $DIR/$THEME/cursors/pencil
-xcursorgen plus.in $DIR/$THEME/cursors/plus
-xcursorgen pointer-move.in $DIR/$THEME/cursors/pointer-move
-xcursorgen question_arrow.in $DIR/$THEME/cursors/question_arrow
-xcursorgen right_ptr.in $DIR/$THEME/cursors/right_ptr
-xcursorgen right_side.in $DIR/$THEME/cursors/right_side
-xcursorgen right_tee.in $DIR/$THEME/cursors/right_tee
-xcursorgen sb_down_arrow.in $DIR/$THEME/cursors/sb_down_arrow
-xcursorgen sb_h_double_arrow.in $DIR/$THEME/cursors/sb_h_double_arrow
-xcursorgen sb_left_arrow.in $DIR/$THEME/cursors/sb_left_arrow
-xcursorgen sb_right_arrow.in $DIR/$THEME/cursors/sb_right_arrow
-xcursorgen sb_up_arrow.in $DIR/$THEME/cursors/sb_up_arrow
-xcursorgen sb_v_double_arrow.in $DIR/$THEME/cursors/sb_v_double_arrow
-xcursorgen tcross.in $DIR/$THEME/cursors/tcross
-xcursorgen top_left_corner.in $DIR/$THEME/cursors/top_left_corner
-xcursorgen top_right_corner.in $DIR/$THEME/cursors/top_right_corner
-xcursorgen top_side.in $DIR/$THEME/cursors/top_side
-xcursorgen top_tee.in $DIR/$THEME/cursors/top_tee
-xcursorgen ul_angle.in $DIR/$THEME/cursors/ul_angle
-xcursorgen ur_angle.in $DIR/$THEME/cursors/ur_angle
-xcursorgen vertical-text.in $DIR/$THEME/cursors/vertical-text
-xcursorgen watch.in $DIR/$THEME/cursors/watch
-xcursorgen X_cursor.in $DIR/$THEME/cursors/X_cursor
-xcursorgen xterm.in $DIR/$THEME/cursors/xterm
-xcursorgen zoom-in.in $DIR/$THEME/cursors/zoom-in
-xcursorgen zoom-out.in $DIR/$THEME/cursors/zoom-out
+xcursorgen crossed_circle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/03b6e0fcb3499374a867c041f52298f0
+xcursorgen left_ptr_watch.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/3ecb610c1bf2410f44200f48c40d3599
+xcursorgen question_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/5c6cd98b3f3ebcb1f9c7f1c204630408
+xcursorgen left_ptr_watch.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/08e8e1c95fe2fc01f976f1e063a24ccd
+xcursorgen hand2.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/9d800788f1b08800ae810202380a0822
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/14fef782d02440884392942c11205230
+xcursorgen link.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/640fb0e74195791501fd1ed57b41487f
+xcursorgen copy.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/1081e37283d90000800003c07f3ef6bf
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/2870a09082c103050810ffdffffe0204
+xcursorgen link.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/3085a0e285430894940527032f8b26df
+xcursorgen move.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/4498f0e0c1937ffe01fd06f973665830
+xcursorgen copy.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/6407b0e94181790501fd1e167b474872
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/028006030e0e7ebffc7f7070c0600140
+xcursorgen move.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/9081237383d90e509aa00f00170e968f
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/00008160000006810000408080010102
+xcursorgen dnd-link.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/alias
+xcursorgen all-scroll.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/all-scroll
+xcursorgen left_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/arrow
+# xcursorgen based_arrow_down.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/based_arrow_down
+# xcursorgen based_arrow_up.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/based_arrow_up
+xcursorgen bd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/bd_double_arrow
+xcursorgen bottom_left_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/bottom_left_corner
+xcursorgen bottom_right_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/bottom_right_corner
+xcursorgen bottom_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/bottom_side
+xcursorgen bottom_tee.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/bottom_tee
+xcursorgen bd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/c7088f0f3e6c8088236ef8e1e3e70000
+xcursorgen crosshair.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/cell
+# xcursorgen center_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/center_ptr
+xcursorgen circle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/circle
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/col-resize
+xcursorgen context-menu.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/context-menu
+xcursorgen copy.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/copy
+xcursorgen cross.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/cross
+xcursorgen crossed_circle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/crossed_circle
+xcursorgen cross.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/crosshair
+xcursorgen cross.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/cross_reverse
+xcursorgen question_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/d9ce0ab605698f320427677b458ad60b
+xcursorgen left_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/default
+xcursorgen cross.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/diamond_cross
+xcursorgen dnd-ask.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-ask
+xcursorgen dnd-copy.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-copy
+xcursorgen dnd-link.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-link
+xcursorgen dnd-move.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-move
+xcursorgen dnd-no-drop.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-no-drop
+xcursorgen dnd-none.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dnd-none
+xcursorgen dotbox.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dotbox
+xcursorgen dotbox.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/dot_box_mask
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/double_arrow
+xcursorgen right_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/draft_large
+xcursorgen right_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/draft_small
+xcursorgen dotbox.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/draped_box
+xcursorgen hand2.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/e29285e634086352946a0e7090d73106
+xcursorgen right_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/e-resize
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ew-resize
+xcursorgen fd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/fcf1c3c7cd4491d801f1e1c78f100000
+xcursorgen fd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/fd_double_arrow
+xcursorgen grabbing.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/fleur
+xcursorgen hand1.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/grab
+xcursorgen grabbing.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/grabbing
+xcursorgen hand2.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/hand
+xcursorgen hand1.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/hand1
+xcursorgen hand2.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/hand2
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/h_double_arrow
+xcursorgen question_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/help
+xcursorgen dotbox.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/icon
+xcursorgen question_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/left_ptr_help
+xcursorgen left_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/left_ptr
+xcursorgen left_ptr_watch.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/left_ptr_watch
+xcursorgen left_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/left_side
+xcursorgen left_tee.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/left_tee
+xcursorgen link.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/link
+xcursorgen ll_angle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ll_angle
+xcursorgen lr_angle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/lr_angle
+xcursorgen move.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/move
+xcursorgen top_right_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ne-resize
+xcursorgen fd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/nesw-resize
+xcursorgen dnd-no-drop.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/no-drop
+xcursorgen crossed_circle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/not-allowed
+xcursorgen top_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/n-resize
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ns-resize
+xcursorgen top_left_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/nw-resize
+xcursorgen bd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/nwse-resize
+xcursorgen pencil.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/pencil
+xcursorgen X_cursor.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/pirate
+xcursorgen plus.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/plus
+xcursorgen hand2.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/pointer
+xcursorgen pointer-move.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/pointer-move
+xcursorgen left_ptr_watch.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/progress
+xcursorgen question_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/question_arrow
+xcursorgen right_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/right_ptr
+xcursorgen right_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/right_side
+xcursorgen right_tee.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/right_tee
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/row-resize
+xcursorgen sb_down_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_down_arrow
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_h_double_arrow
+xcursorgen sb_left_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_left_arrow
+xcursorgen sb_right_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_right_arrow
+xcursorgen sb_up_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_up_arrow
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sb_v_double_arrow
+xcursorgen top_left_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/se-resize
+xcursorgen dnd-none.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/size_all
+xcursorgen fd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/size_bdiag
+xcursorgen bd_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/size_fdiag
+xcursorgen sb_h_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/size_hor
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/size_ver
+xcursorgen top_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/s-resize
+xcursorgen top_right_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/sw-resize
+xcursorgen dotbox.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/target
+xcursorgen tcross.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/tcross
+xcursorgen xterm.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/text
+xcursorgen left_ptr.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/top_left_arrow
+xcursorgen top_left_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/top_left_corner
+xcursorgen top_right_corner.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/top_right_corner
+xcursorgen top_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/top_side
+xcursorgen top_tee.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/top_tee
+xcursorgen ul_angle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ul_angle
+xcursorgen ur_angle.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/ur_angle
+xcursorgen sb_v_double_arrow.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/v_double_arrow
+xcursorgen vertical-text.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/vertical-text
+xcursorgen watch.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/watch
+xcursorgen right_side.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/w-resize
+xcursorgen X_cursor.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/X_cursor
+xcursorgen xterm.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/xterm
+xcursorgen zoom-in.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/zoom-in
+xcursorgen zoom-out.in $DIR/"$THEME"/out/X11/"$THEME"/cursors/zoom-out
 
-# go back up one
-cd ..
+#if script generate error or success
+if [ $? -ne 0 ]
+    then
+		exit 1
+fi
+# go back up two
+cd ../../
