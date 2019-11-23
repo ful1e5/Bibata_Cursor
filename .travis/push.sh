3bfb6fdf05e98b28374e3c131e04996d2ff26c68
Travis CI <travis@travis-ci.org>
2019-11-23 17:14:19 +0530

    upload script changed

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_build_files() {
-  git checkout -b travis-build
+  git checkout -b test
   git add . 
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/KaizIqbal/Bibata_Cursor.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin travis-build
+  git push --quiet --set-upstream origin test
 }
 
 setup_git
