e08e777579869c0fe41ab99056288049ba912e31
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-15 09:36:55 +0530

    💁 Helpers function for html template generation

---
+++ packages/shared/src/helpers/htmlTemplate.ts
@@ -0,0 +1,19 @@
+export const template = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Eggy Render Template</title>
+    </head>
+                
+    <body>
+        <div id="container">
+            <svginjection>
+        </div>
+    </body>
+</html>
+`;
+
+export const generateRenderTemplate = (svg: string) =>
+  template.replace("<svginjection>", svg);
