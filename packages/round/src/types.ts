c832ae265f6ec69d3cf6bf9de558d8c1785573c2
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-16 12:42:20 +0530

    🏷️ Config type

--- packages/round/src/types.ts
+++ packages/round/src/types.ts
@@ -1,6 +1,29 @@
-export interface ColorSchema {
+interface ColorSchema {
   [name: string]: {
     base: string;
     outline: string;
   };
 }
+
+type AnimatedCursors = {
+  readonly [name: string]: {
+    readonly frames: number;
+  };
+};
+
+type AnimatedClip = {
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+};
+
+interface Config {
+  staticSvgs: Array<string>;
+  bitmapsDir: string;
+  svgsDir: string;
+  animatedCursors: AnimatedCursors;
+  animatedClip: AnimatedClip;
+}
+
+export { ColorSchema, Config };
