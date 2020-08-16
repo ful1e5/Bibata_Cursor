2a52a2334bb9bc4526a215ac4538184018b127f3
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-16 12:19:50 +0530

    🤙 helper call

--- packages/round/src/config.ts
+++ packages/round/src/config.ts
@@ -1,14 +1,13 @@
 import fs from "fs";
 import path from "path";
-import rimraf from "rimraf";
 
-import { staticCursors, animatedCursors, animatedClip } from "./cursors.json";
+import { generateConfigs } from "./helpers/schema";
 import { ColorSchemas } from "./types";
 
 // Source Directory
-export const rawSvgsDir = path.resolve("./src/svg/raw");
-if (!fs.existsSync(rawSvgsDir))
-  console.error("🚨🚨 Source files not Found 🚨🚨");
+const rawSvgsDir = path.resolve("./src/svg/raw");
+
+if (!fs.existsSync(rawSvgsDir)) console.error("🚨🚨 Raw files not Found 🚨🚨");
 
 // --------------------------------------- 🌈 Cursors Variants 🌈
 
@@ -20,50 +19,6 @@ const colorSchemes: ColorSchemas = {
 };
 
 // --------------------------------------- 🔧 Render Configs 🔧
-const configs: any = [];
+const configs = generateConfigs(colorSchemes);
 
-for (let [schema] of Object.entries(colorSchemes)) {
-  const { base, outline } = colorSchemes[schema];
-  const schemaName = `Bibata_${schema}`;
-
-  const schemaSvgsPath = path.resolve("./src/svg", schemaName);
-
-  if (fs.existsSync(schemaSvgsPath)) rimraf(schemaSvgsPath, function () {});
-  fs.mkdirSync(schemaSvgsPath);
-
-  // Resolve Paths for svg
-  const staticSvgs = staticCursors.map((svgFile: string) => {
-    // Read file
-    const filePath = path.resolve(schemaSvgsPath, svgFile);
-
-    // Replacing colorSchema
-    let content = fs
-      .readFileSync(path.resolve(rawSvgsDir, svgFile), "utf-8")
-      .toString();
-
-    content = content
-      .replace("black", base)
-      .replace("white", outline)
-      .replace("#000000", base)
-      .replace("#ffffff", outline);
-
-    // Writing new svg
-    fs.writeFileSync(filePath, content, "utf-8");
-
-    return filePath;
-  });
-
-  // Out Directory
-  const bitmapsDir = path.resolve(process.cwd(), "bitmaps", `Bibata_${schema}`);
-  if (!fs.existsSync(bitmapsDir)) fs.mkdirSync(bitmapsDir);
-
-  configs.push({
-    svgDir: schemaSvgsPath,
-    staticSvgs,
-    bitmapsDir,
-    animatedCursors,
-    animatedClip
-  });
-}
-
-export { configs };
+export { rawSvgsDir, configs };
