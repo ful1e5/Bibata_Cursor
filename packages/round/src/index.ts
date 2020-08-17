dc82fa38d97c62014b687df7a07f57b15ffdb45a
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-17 11:55:26 +0530

    💬 Comments

--- packages/round/src/index.ts
+++ packages/round/src/index.ts
@@ -1,20 +1,21 @@
+import { renderCursors } from "shared";
+
 import { generateConfigs } from "./helpers/schema";
 import { colorSchemes, rawSvgsDir } from "./color";
 
-// from shared package
-import { renderCursors } from "shared";
-
 (async () => {
-  // Svg generate dir prefix
   const prefix = "Bibata-Round";
   const configs = generateConfigs(colorSchemes, prefix, rawSvgsDir);
 
   try {
     for (let [schema, { bitmapsDir }] of Object.entries(configs)) {
       console.log(`\nGenerating ${prefix}-${schema} bitmaps..`);
+
       await renderCursors(configs[schema]);
+
       console.log(`\nBitmaps stored at ${bitmapsDir}`);
     }
+
     console.log("\n\n🎉 Render Done.");
   } catch (error) {
     console.error(error);
