0fdc2b327a528eb392d85b8d2349efeb0949ad98
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-25 11:21:21 +0530

    ↕️ ↔️ Arrows Cursor fix

--- config.py
+++ config.py
@@ -29,9 +29,9 @@ with open("./package.json") as f:
 # Windows Cursors Config
 windows_cursors = {
     "all-scroll.cur": "move.cur",
-    "bd_double_arrow.cur": "diagonal-resize-1.cur",
-    "bottom_left_corner.cur": "diagonal-resize-2.cur",
-    "bottom_side.cur": "vertical-resize.cur",
+    "fd_double_arrow.cur": "diagonal-resize-1.cur",
+    "bd_double_arrow.cur": "diagonal-resize-2.cur",
+    "sb_h_double_arrow.cur": "vertical-resize.cur",
     "circle.cur": "unavailable.cur",
     "crosshair.cur": "precision-select.cur",
     "dnd-ask.cur": "help-select.cur",
@@ -39,7 +39,7 @@ windows_cursors = {
     "left_ptr.cur": "normal-select.cur",
     "left_ptr_watch.ani": "working-in-background.ani",
     "pencil.cur": "handwriting.cur",
-    "right_side.cur": "horizontal-resize.cur",
+    "sb_h_double_arrow.cur": "horizontal-resize.cur",
     "sb_up_arrow.cur": "alt-select.cur",
     "wait.ani": "busy.ani",
     "x_cursor.cur": "pirate.cur",
