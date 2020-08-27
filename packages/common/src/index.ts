8858bb0ea37dc9b01d31c18a8971022539606227
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-27 17:31:36 +0530

    🚛 Export types

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,5 @@
 import { renderCursors } from "./utils/render";
 import { spinner } from "./utils/spinner";
+import * as types from "./types";
 
-export { spinner, renderCursors };
+export { spinner, renderCursors, types };
