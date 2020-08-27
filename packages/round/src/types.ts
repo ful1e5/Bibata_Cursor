5b329fa90692295a8085c21828a04b55ab3a66dc
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-27 17:41:48 +0530

    🏷️ Shared Types

--- packages/round/src/types.ts
+++ packages/round/src/types.ts
@@ -1,3 +1,5 @@
+import { Config } from "common/src/types";
+
 interface ColorSchema {
   [name: string]: {
     base: string;
@@ -8,29 +10,4 @@ interface ColorSchema {
   };
 }
 
-type AnimatedCursors = {
-  readonly [name: string]: {
-    readonly frames: number;
-  };
-};
-
-type AnimatedClip = {
-  readonly x: number;
-  readonly y: number;
-  readonly width: number;
-  readonly height: number;
-};
-
-interface Config {
-  staticCursors: Array<string>;
-  bitmapsDir: string;
-  svgsDir: string;
-  animatedCursors: AnimatedCursors;
-  animatedClip: AnimatedClip;
-}
-
-interface Configs {
-  [name: string]: Config;
-}
-
-export { ColorSchema, Config, Configs };
+export { ColorSchema, Config };
