117152a52cb7abb32153ff049e85819e6a48942c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-31 12:09:13 +0530

    🏷️ types update

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,14 +1,7 @@
-import { BoundingBox } from "puppeteer";
-
-type AnimatedCursors = Record<string, { frames: number }>;
-
-type AnimatedClip = BoundingBox | undefined;
-
 interface Config {
   staticCursors: string[];
+  animatedCursors: string[];
   bitmapsDir: string;
-  svgsDir: string;
-  animatedCursors: AnimatedCursors;
 }
 
-export { Config, AnimatedCursors, AnimatedClip };
+export { Config };
