155dac47a8a53d815caaa41c58ecb444ff9ac13c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-31 12:39:14 +0530

    🔑 Frame key utils

---
+++ packages/common/src/utils/getKeyName.ts
@@ -0,0 +1,14 @@
+import path from "path";
+
+export const frameNumber = (index: number, endIndex: number) => {
+  let result = "" + index;
+  while (result.length < endIndex) {
+    result = "0" + result;
+  }
+  return result;
+};
+
+export const getKeyName = (index: number, fileName: string) => {
+  const frame = frameNumber(index, 2);
+  return `${path.basename(fileName, ".svg")}-${frame}.png`;
+};
