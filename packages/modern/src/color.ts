c5ebeb06d2bd38dedbcd12e3446afda70886b62a
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-02 09:54:17 +0530

    🌈 Unlock all color schemes

--- packages/modern/src/color.ts
+++ packages/modern/src/color.ts
@@ -5,27 +5,27 @@ const black = "#000000";
 const white = "#FFFFFF";
 
 // Schemes Colors
-// const adwaita = "#272728";
-// const amber = "#FF8300";
-// const breezeDark = "#4A4C49";
+const adwaita = "#272728";
+const amber = "#FF8300";
+const breezeDark = "#4A4C49";
 
 const colorSchemes: ColorSchemes = {
-  // Adwaita: {
-  //   base: adwaita,
-  //   outline: white
-  // },
-  // Amber: {
-  //   base: amber,
-  //   outline: white
-  // },
-  // Breeze: {
-  //   base: breezeDark,
-  //   outline: white
-  // },
-  // Classic: {
-  //   base: black,
-  //   outline: white
-  // },
+  Adwaita: {
+    base: adwaita,
+    outline: white
+  },
+  Amber: {
+    base: amber,
+    outline: white
+  },
+  Breeze: {
+    base: breezeDark,
+    outline: white
+  },
+  Classic: {
+    base: black,
+    outline: white
+  },
   Ice: {
     base: white,
     outline: black
