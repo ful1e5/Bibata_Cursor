05ad26eb12d994094fb9091541966a7bac48a08e
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-02 16:36:33 +0530

    🌈 Cleanup color Schemes

--- packages/modern/src/color.ts
+++ packages/modern/src/color.ts
@@ -3,27 +3,15 @@ import { ColorSchemes } from "bibata-core/src/types";
 // Common Colors
 const black = "#000000";
 const white = "#FFFFFF";
-
-// Schemes Colors
-const adwaita = "#272728";
 const amber = "#FF8300";
-const breezeDark = "#4A4C49";
 
 const colorSchemes: ColorSchemes = {
-  Adwaita: {
-    base: adwaita,
-    outline: white
-  },
   Amber: {
     base: amber,
     outline: white,
     watch: {
       background: white
     }
-  },
-  Breeze: {
-    base: breezeDark,
-    outline: white
   },
   Classic: {
     base: black,
