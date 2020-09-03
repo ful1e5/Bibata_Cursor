b15ef228b6246dbacce59f286affbaff591ffcb0
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-03 19:35:03 +0530

    🔗 all_scroll.svg links added

--- packages/modern/src/svg/symblinks.sh
+++ packages/modern/src/svg/symblinks.sh
@@ -3,9 +3,10 @@
 cd ./static
 
 # remove files
-rm -rf dnd_none.svg
+rm -rf dnd_none.svg all_scroll.svg
 
 # creating symbolic links
 ln -s ./move.svg ./dnd_none.svg
+ln -s ./move.svg ./all_scroll.svg
 
 cd ..
