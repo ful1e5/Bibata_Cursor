5c1d89c2825cd3a63825fc587c0cd82cecb38b2e
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-27 16:58:35 +0530

    ⚡ Render Template name changed

--- packages/core/src/utils/htmlTemplate.ts
+++ packages/core/src/utils/htmlTemplate.ts
@@ -4,7 +4,7 @@ export const template = `
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Eggy Render Template</title>
+        <title>Bibata Cursors Render Template</title>
     </head>
                 
     <body>
