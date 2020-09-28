af9294ad0dde5ddb2ca48959b1a9415bcf89206a
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-28 20:35:33 +0530

    📝 TODO

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,9 +2,11 @@ import { ThemeColors } from "./types";
 
 const createBitmaps = async (config: {
   themeColors: ThemeColors;
+  svgDir: string;
   themeName: string;
   bitmapsDir: string;
 }) => {
+  // TODO:
   console.log(config);
 };
 
