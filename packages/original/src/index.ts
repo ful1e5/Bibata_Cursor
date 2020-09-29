bd9332180749c912265176e04027426473ed87d6
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-09-29 10:43:44 +0530

    🚀 Main method with try&catch block

--- packages/original/src/index.ts
+++ packages/original/src/index.ts
@@ -2,12 +2,21 @@ import { createBitmaps } from "bibata-core";
 import { themeColors } from "./colors";
 import { resolve } from "path";
 
-const projectRoot = resolve("../../");
-const args = {
-  themeName: "Bibata-Modern",
-  svgDir: resolve("./src/svg"),
-  bitmapsDir: resolve(projectRoot, "bitmaps"),
-  themeColors
+const main = async () => {
+  const projectRoot = resolve("../../");
+  const args = {
+    themeName: "Bibata-Original",
+    svgDir: resolve("./src/svg"),
+    bitmapsDir: resolve(projectRoot, "bitmaps"),
+    themeColors
+  };
+
+  try {
+    await createBitmaps(args);
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-createBitmaps(args);
+main();
