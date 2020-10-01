15a9bd71dccb13f54dd211d4650dee8d3e0728d5
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-01 17:49:25 +0530

    📚 Types Docs

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -4,22 +4,40 @@ interface Frames {
   };
 }
 
+/**
+ * Hex Colors in string Formate.
+ *
+ * `Example: `"#FFFFFF"
+ */
+type HexColor = string;
+
+/**
+ * @Colors expect `base`, `outline` & `watch-background` colors in **HexColor** Format.
+ * @default background is `base` color.
+ */
 type Colors = {
-  base: string;
-  outline: string;
+  base: HexColor;
+  outline: HexColor;
   watch?: {
-    background: string;
+    background: HexColor;
   };
 };
 
+/**
+ * Colors object for this cursors theme.
+ *
+ * @example
+ * ```typescript
+ * const themeColors: ThemeColors = {
+ *   White:{
+ *     base: "#FFFFFF",
+ *     outline: "#000000"
+ *   },
+ * };
+ * ```
+ */
 interface ThemeColors {
   [themeName: string]: Colors;
 }
 
-interface PixelDiffRate {
-  [name: string]: {
-    rate: number;
-  };
-}
-
-export { Frames, Colors, ThemeColors, PixelDiffRate };
+export { Frames, Colors, ThemeColors };
