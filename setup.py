2512d40fe8075fc4c7e3df01dd2c321f0e8c7fce
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-10-24 10:02:54 +0530

    📦 Lock pip requirements

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from builder.pkg_info import info
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name=info["name"],
+    name=info["pkg_name"],
     version=info["version"],
     description=info["description"],
     url=info["url"],
     author=info["author"],
     author_email=info["email"],
-    install_requires=["Pillow>=7.2.0", "clickgen>=1.1.7"],
+    install_requires=["Pillow>=7.2.0", "clickgen==1.1.7"],
     packages=find_namespace_packages(include=["builder"]),
     classifiers=[
         "Programming Language :: Python :: 3",
