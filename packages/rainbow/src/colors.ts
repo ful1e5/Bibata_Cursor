da072a031afe6a0db42e99944ce15926bd6fff9c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-12 10:25:16 +0530

    📦 Init bibata-rainbow

---
+++ packages/rainbow/src/colors.ts
@@ -0,0 +1,14 @@
+import { ThemeColors } from "bibata-core/src/types";
+
+// Colors
+const none = "#00FF00";
+const black = "#000000";
+
+const themeColors: ThemeColors = {
+  Rainbow: {
+    base: none, // .svg base by default animated
+    outline: black
+  }
+};
+
+export { themeColors };
