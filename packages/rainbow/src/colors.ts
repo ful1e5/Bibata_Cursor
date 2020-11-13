742f150b7257b3c01ae343305423c4ded7363e06
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-11-13 10:00:00 +0530

    🌈 All black colors

--- packages/rainbow/src/colors.ts
+++ packages/rainbow/src/colors.ts
@@ -1,12 +1,11 @@
 import { ThemeColors } from "bibata-core/src/types";
 
 // Colors
-const none = "#00FF00";
 const black = "#000000";
 
 const themeColors: ThemeColors = {
   Rainbow: {
-    base: none, // .svg base by default animated
+    base: black, // .svg base by default animated
     outline: black
   }
 };
